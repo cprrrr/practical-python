ab0da87e81394fd8e493a450fa3e1dbc19e3dc4b
cpr <jyl12321@hotmail.com>
2025-06-03 16:44:57 +0800

    up to exercise 2.12

--- Work/report.py
+++ Work/report.py
@@ -16,7 +16,7 @@ def read_portfolio(filename):
                 price = float(row[2])
                 portfolio.append({'name' : tickle, 'shares' : num, 'price' : price})
             except ValueError:
-                print('something is wrong->', row)
+                print('something is wrong in '+str(filename)+'->', row)
                 continue
     return portfolio
 
@@ -30,9 +30,16 @@ def read_endprice(filename):
                 p = float(row[1])
                 dic[name] = p
             except (ValueError, IndexError) as error:
-                print('something is wrong->', row)
+                print('something is wrong in '+str(filename)+'->', row)
                 continue
     return dic
+
+def make_report(lisostock, dicoprice):
+    rows = []
+    for stock in lisostock:
+        change = float(dicoprice[stock['name']] - stock['price'])
+        rows.append((stock['name'], stock['shares'], dicoprice[stock['name']], change))
+    return rows
 
 if len(sys.argv) == 2:
     file = sys.argv[1]
@@ -40,15 +47,15 @@ else:
     file = 'D:/adastra/playground/pythontuto/practical-python/Work/Data/portfolio.csv'
 portfolio = read_portfolio(file)
 endprice = read_endprice('Data/prices.csv')
+table = make_report(portfolio, endprice)
 
-sp = 0
-ep = 0
-for stock in portfolio:
-    stock['endprice'] = endprice[stock['name']]
-    sp += stock['shares'] * stock['price']
-    ep += stock['shares'] * stock['endprice']
-    print(stock)
+print(f"\n{'Name':>10s} {'Shares':>10s} {'Price':>10s} {'Change':>10s}")
+print(('-' * 10 + ' ') * 4)
+gl = 0
+for i in table:
+    print(f"{i[0]:>10s} {i[1]:>10d} {'$'+str(round(i[2], 2)):>10s} {'$'+str(round(i[3], 2)):>10s}")
+    gl += i[1] * i[3]
 
-print(f'total gain/loss:{ep - sp:0.2f}')
+print(f'\ntotal gain/loss: ${gl:0.2f}')
 
 
