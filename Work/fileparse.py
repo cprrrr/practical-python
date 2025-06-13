82a43e86957d80b8bcf65a6a280fbcad1da25f25
cpr <jyl12321@hotmail.com>
2025-06-13 18:34:05 +0800

    up to 3.3 error checking

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,16 +3,13 @@
 # Exercise 3.3
 
 import csv
+from pprint import pprint
 
-def csv_parse(filename: str, select: list = None, types: list = None, delimit: str = None,has_headers = True) -> list:
-    '''
-    parse a csv file into a list of records (list of dicts)
-    :param filename:
-    :param select:
-    :param types:
-    :param delimit:
-    :return:
-    '''
+def csv_parse(filename: str, select: list = None, types: list = None, delimit: str = None,has_headers = True, silence_errors=False) -> list:
+
+    if select and not has_headers:
+        raise RuntimeError("select argument requires column headers")
+
     with open(filename) as f:
         if delimit:  # if specify delimiter
             rows = csv.reader(f, delimiter=delimit)
@@ -26,27 +23,47 @@ def csv_parse(filename: str, select: list = None, types: list = None, delimit: s
                 pass
             else:
                 select = headers
-            try:
                 indices = [headers.index(x) for x in select]
-            except ValueError:
-                print("file only contains:", indices)
 
-            for row in rows:  # work through
+            for no, row in enumerate(rows):  # work through
                 if not row:  # skip blank row
                     continue
                 else:
-                    if types:
-                        record = {headers[x]: t(row[x]) for t, x in zip(types, indices)}
-                    else:
-                        record = {headers[x]: row[x] for x in indices}
-                    records.append(record)
+                    try:
+                        if types:
+                            record = {headers[x]: t(row[x]) for t, x in zip(types, indices)}
+                        else:
+                            record = {headers[x]: row[x] for x in indices}
+                        records.append(record)
+                    except ValueError as e:
+                        if silence_errors:
+                            continue
+                        else:
+                            print(f"Row {no:d}: Couldn't convert {row} \nReason: {e}")
+                            continue
 
         else: # without headers
-            for row in rows:  # work through
+            for no, row in enumerate(rows):  # work through
                 if not row:  # skip blank row
                     continue
                 else:
-                    record = tuple([a(b) for a, b in zip(types, row)])
-                    records.append(record)
+                    try:
+                        record = tuple([a(b) for a, b in zip(types, row)])
+                        records.append(record)
+                    except ValueError as e:
+                        if silence_errors:
+                            continue
+                        else:
+                            print(f"Row {no:d}: Couldn't convert {row} \nReason: {e}")
+                            continue
 
     return records
+
+def main(filename: str, select: list = None, types: list = None, delimit: str = None,has_headers = True, silence_errors=False):
+    portfolio = csv_parse(filename, select, types, delimit, has_headers, silence_errors)
+    pprint(portfolio)
+    return None
+
+main('Data/missing.csv', types=[str,int,float])
+
+
