8969bb1184e5938de025590fa4d30d8581f133d4
cpr <jyl12321@hotmail.com>
2025-06-17 01:46:48 +0800

    up to 4.2inheritance exercise4.8

--- Work/report.py
+++ Work/report.py
@@ -4,51 +4,59 @@
 import sys
 import fileparse
 import stock
+import tableformat
 
 
-def read_portfolio(filename: str, select: list = None, types: list = None, delimit: str = None, silence_errors=False)->list:
+def read_portfolio(filename: str, select: list = None, types: list = None, delimit: str = None,
+                   silence_errors=False) -> list:
     with open(filename, 'rt') as f:
-        portfolio = fileparse.parse_csv(iname=f, select=select, types=types, delimit=delimit, has_headers=True, silence_errors=silence_errors)
+        portfolio = fileparse.parse_csv(iname=f, select=select, types=types, delimit=delimit, has_headers=True,
+                                        silence_errors=silence_errors)
     stocklis = [stock.Stock(i['name'], i['shares'], i['price']) for i in portfolio]
     return stocklis
 
-def read_endprice(filename: str, select: list = None, types: list = None, delimit: str = None, silence_errors=False)->dict:
+
+def read_endprice(filename: str, select: list = None, types: list = None, delimit: str = None,
+                  silence_errors=False) -> dict:
     with open(filename, 'rt') as f:
-        dic = fileparse.parse_csv(iname=f, select=select, types=types, delimit=delimit, has_headers=False, silence_errors=silence_errors)
+        dic = fileparse.parse_csv(iname=f, select=select, types=types, delimit=delimit, has_headers=False,
+                                  silence_errors=silence_errors)
     dic = dict(dic)
     return dic
 
-def make_report(lisostock: list, dicoprice: dict)->list:
+
+def make_report(lisostock: list, dicoprice: dict) -> list:
     rows = []
     for stock in lisostock:
         change = float(dicoprice[stock.name] - stock.price)
         rows.append((stock.name, stock.shares, dicoprice[stock.name], change))
     return rows
 
-def print_report(tab: list):
-    print(f"\n{'Name':>10s} {'Shares':>10s} {'Price':>10s} {'Change':>10s}")
-    print(('-' * 10 + ' ') * 4)
-    gl = 0
-    for i in tab:
-        print(f"{i[0]:>10s} {i[1]:>10d} {'$' + str(round(i[2], 2)):>10s} {'$' + str(round(i[3], 2)):>10s}")
-        gl += i[1] * i[3]
-    print(f'\ntotal gain/loss: ${gl:0.2f}')
 
-def portfolio_report(filename: str, pricefilename='Data/prices.csv'):
-    portfolio = read_portfolio(filename, types = [str, int, float])
-    endprice = read_endprice(pricefilename, types = [str, float])
+def print_report(tab: list, formatter: tableformat.TableFormatter):
+    formatter.headings(['Name', 'Shares', 'Price', 'Change'])
+    for name, shares, price, change in tab:
+        rowdata = [name, str(shares), f'{price:0.2f}', f'{change:0.2f}']
+        formatter.row(rowdata)
+
+
+def portfolio_report(filename: str, pricefilename='Data/prices.csv', fmt: str = 'txt'):
+    portfolio = read_portfolio(filename, types=[str, int, float])
+    endprice = read_endprice(pricefilename, types=[str, float])
     table = make_report(portfolio, endprice)
-    print_report(table)
+
+    formatter = tableformat.create_formatter(fmt)
+    print_report(table, formatter)
+
 
 def main(argv: list):
     f1 = str(argv[1])
     f2 = str(argv[2])
-    portfolio_report(f1, f2)
+    fom = str(argv[3])
+    portfolio_report(f1, f2, fom)
 
 
 if __name__ == '__main__':
     import sys
+
     main(sys.argv)
-
-
-
---
+++ Work/tableformat.py
@@ -0,0 +1,60 @@
+# tableformat.py
+
+class TableFormatter(object):
+    def headings(self, headers):
+        raise NotImplementedError()
+
+    def row(self, rowdata):
+        raise NotImplementedError()
+
+class TextTableFormatter(TableFormatter):
+    '''
+    Emit a table in plain-text format
+    '''
+    def headings(self, headers: list):
+        for i in headers:
+            print(f"{i:>10s}", end=' ')
+        print('')
+        print(('-'*10+' ')*len(headers))
+
+    def row(self, rowdata: list):
+        for i in rowdata:
+            print(f"{i:>10s}", end=' ')
+        print('')
+
+class CSVTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in CSV format.
+    '''
+    def headings(self, headers):
+        print(','.join(headers))
+
+    def row(self, rowdata):
+        print(','.join(rowdata))
+
+class HTMLTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print('<tr>', end='')
+        for i in headers:
+            print(f"<th>{i}</th>", end='')
+        print('</tr>')
+
+    def row(self, rowdata):
+        print('<tr>', end='')
+        for i in rowdata:
+            print(f"<td>{i}</td>", end='')
+        print('</tr>')
+
+def create_formatter(name: str):
+    if name == 'txt':
+        formatter = TextTableFormatter()
+    elif name == 'csv':
+        formatter = CSVTableFormatter()
+    elif name == 'html':
+        formatter = HTMLTableFormatter()
+    else:
+        raise RuntimeError(f'Unknown format {fmt}')
+
+    return formatter
+
+
